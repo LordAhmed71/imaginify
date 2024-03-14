f598455c642f842cef2b632280f62feae87656fc
LordAhmed71 <ahmed55mohamed88@gmail.com>
2024-03-14 23:05:28 +0200

    add sidebar and header for small screens

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,12 @@
 import { IAuthLayout } from "@/types";
+import Sidebar from "./../../components/shared/Sidebar";
+import MobileNav from "@/components/shared/MobileNav";
 
 const Layout = ({ children }: IAuthLayout) => {
   return (
     <main className="root">
+      <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
