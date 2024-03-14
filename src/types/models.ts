2ea337954f9e29732698f8e8593eca52bafbb756
LordAhmed71 <ahmed55mohamed88@gmail.com>
2024-03-15 00:28:40 +0200

    make the modals and setup the mongo db connection

---
+++ src/types/models.ts
@@ -0,0 +1,22 @@
+import { Document } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureURL: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: string;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
